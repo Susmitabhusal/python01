53e4060ae0ec25e99aab2cb81c2b5381bbee31bd
Susmitabhusal <susmitabhusal65@gmail.com>
2021-04-13 14:10:28 +0545

    Added my project

---
+++ function_test/test1.py
@@ -0,0 +1,32 @@
+from my_libs import Myfunctions
+
+"""
+for i in range(10):
+    Myfunctions.f1()  # call the function
+"""
+
+"""
+var1="Welcome to Broad way Info sys"
+Myfunctions.f2(var1)
+"""
+
+# f2(num1, num2) # function signature- function name and list of parameters
+"""
+n1 =5
+n2 = 8
+Myfunctions.f2(n1, n2)
+
+n1 =int(input("enter first no:"))
+n2 =int(input("enter second no:"))
+Myfunctions.f2(n1, n2)
+
+"""
+num1 = Myfunctions.f3()
+print(num1)
+
+num2 = Myfunctions.f4()
+print(num2)
+
+num3 = Myfunctions.f5(12, 13)
+print(num3)
+
---
+++ my_libs/Myfunctions.py
@@ -0,0 +1,37 @@
+def f1():
+    print("hello from f1 function")
+    print("Name:Susmita")
+    print("Address:kathmandu")
+    print("Email:abc@gmail.com")
+
+
+"""
+def f2(var2):
+    print("this is function 2")
+    print(var2)
+"""
+
+
+def f2(num1, num2):
+    num3 = num1 + num2
+    print("Num1:", num1)
+    print("Num2:", num2)
+    print("sum:", num3)
+
+
+def f3():
+    tmp = int(input("Enter any numbers:"))
+    return tmp
+
+
+def f4():
+    num1 = 12
+    num2 = 23
+    num3 = num1 + num2
+    return num3
+
+
+def f5(num1, num2):
+    num3 = num1 + num2
+    return num3
+
