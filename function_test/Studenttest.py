6df116b4834dc183c363649c77c2088239760134
Susmitabhusal <susmitabhusal65@gmail.com>
2021-04-15 14:23:36 +0545

    Student function created

--- function_test/Studenttest.py
+++ function_test/Studenttest.py
@@ -1,11 +1,29 @@
 from my_libs import StudentFunction
 
 var1 = StudentFunction.f1()
-
-
 var2 = StudentFunction.f2()
-print(var1)
-print(var2)
-
 var3 = StudentFunction.f3()
-print(var3)
+var4 = StudentFunction.f4()
+var5 = StudentFunction.f5()
+var6 = StudentFunction.f6()
+var7 = StudentFunction.f7()
+var8 = StudentFunction.f8()
+var9 = StudentFunction.f9()
+print("Roll no  : ", var1)
+print("Name     : ", var2)
+print("Class     : ", var3)
+print("Section     : ", var4)
+print("Marks in ")
+print("sub 1     : ", var5)
+print("sub 2     : ", var6)
+print("sub 3     : ", var7)
+print("sub4     : ", var8)
+print("sub 5     : ", var9)
+
+var10 = StudentFunction.f10(var5, var6, var7, var8, var9)
+print("Total is  :",var10)
+
+var11 = StudentFunction.f11(var10)
+print("Average is  :", var11)
+
+StudentFunction.f12(var11)
--- my_libs/StudentFunction.py
+++ my_libs/StudentFunction.py
@@ -4,7 +4,7 @@ def f1():
 
 
 def f2():
-    tmp2 = int(input("enter the name of student"))
+    tmp2 = str(input("enter the name of student"))
     return (tmp2)
 
 
@@ -14,30 +14,65 @@ def f3():
 
 
 def f4():
-    tmp4 = int(input("enter the section of student"))
+    tmp4 = str(input("enter the section of student"))
     return (tmp4)
 
 
 def f5():
-    tmp5 = int(input("enter the marks of first subject"))
+    tmp5 = float(input("enter the marks of first subject"))
     return (tmp5)
 
 
 def f6():
-    tmp6 = int(input("enter the marks of second subject"))
+    tmp6 = float(input("enter the marks of second subject"))
     return (tmp6)
 
 
 def f7():
-    tmp7 = int(input("enter the marks of third subject"))
+    tmp7 = float(input("enter the marks of third subject"))
     return (tmp7)
 
 
 def f8():
-    tmp8 = int(input("enter the marks of fourth subject"))
+    tmp8 = float(input("enter the marks of fourth subject"))
     return (tmp8)
 
 
 def f9():
-    tmp9 = int(input("enter the marks of fifth subject"))
+    tmp9 = float(input("enter the marks of fifth subject"))
     return (tmp9)
+
+
+def f10(s1, s2, s3, s4, s5):
+    total = s1 + s2 + s3 + s4 + s5
+    return (total)
+
+
+def f11(total):
+    avg = total / 5
+    return avg
+
+
+def f12(avg):
+    if avg >= 91 and avg <= 100:
+        print("Grade = A1")
+    elif avg >= 81 and avg < 91:
+        print("Grade = A2")
+    elif avg >= 71 and avg < 81:
+        print("Grade = B1")
+    elif avg >= 61 and avg < 71:
+        print("Grade = B2")
+    elif avg >= 51 and avg < 61:
+        print("Grade = C1")
+    elif avg >= 41 and avg < 51:
+        print("Grade = C2")
+    elif avg >= 33 and avg < 41:
+        print("Grade = D")
+    elif avg >= 21 and avg < 33:
+        print("Grade = E1")
+    elif avg >= 0 and avg < 21:
+        print("Grade = E2")
+    else:
+        print("Invalid Input!")
+
+
