e56f0dca3a460cba4565df3bdc96448beed0d397
Susmitabhusal <susmitabhusal65@gmail.com>
2021-04-20 06:43:44 +0545

    Inheritence and default constructors

--- OOPS/RClass.py
+++ OOPS/RClass.py
@@ -64,3 +64,25 @@ class Class4():
     #__str__
     def __str__(self):
         return str(self.id) + "," +self.name +"," +self.address
+
+#default Constructors
+class Class5():
+    def __init__(self,id=0):
+        self.id= id
+    def getId(self):
+        return str(self.id)
+    def setId(self,id):
+        self.id= id
+    def __str__(self):
+        return str(self.id)
+#Inheritence
+class Class6(Class5):
+    def __init__(self,id=0,fullName="NA"):
+        super().__init__(id)
+        self.fullName=fullName
+    def getValue(self):
+        return self.fullName
+    def setValue(self,fullName):
+        self.fullName =fullName
+    def __str__(self):
+        return super(Class6, self).__str__()+"," +self.fullName
